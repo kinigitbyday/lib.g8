faac2e500232adfd68c8e5c6f9a016fdc859c953
anton <akropp@gmail.com>
2017-11-15 18:13:28 -0800

    Fixing revision

--- src/main/g8/deploy.sh
+++ src/main/g8/deploy.sh
@@ -1,7 +1,7 @@
 function snapshot() {
-  sbt -Drevision="\$TRAVIS_BUILD_NUMBER-SNAPSHOT" publishSigned
+  sbt -Dversion="\${TRAVIS_BUILD_NUMBER}-SNAPSHOT" publishSigned
 }
 
 function release() {
-  sbt -Drevision=\$REVISION publishSigned sonatypeRelease
+  sbt -Dversion=\$REVISION publishSigned sonatypeRelease
 }
