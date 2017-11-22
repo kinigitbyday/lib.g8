cbb3b0d04ee3cf478ca451040af2e3a2aecdd848
anton <akropp@gmail.com>
2017-11-22 12:21:51 -0800

    Fixing publishing

--- src/main/g8/project/Sonatype.scala
+++ src/main/g8/project/Sonatype.scala
@@ -5,11 +5,6 @@ import xerial.sbt.Sonatype.SonatypeKeys._
 
 object Publishing {
   val publishSettings = Seq(
-    credentials += Credentials("Sonatype Nexus Repository Manager",
-      "oss.sonatype.org",
-      sys.env.getOrElse("SONATYPE_USER", default = "paradoxicalio"),
-      sys.env.getOrElse("SONATYPE_PASSWORD", default = "")),
-
     publishMavenStyle := true,
 
     publishTo := Some(
@@ -20,12 +15,6 @@ object Publishing {
     ),
 
     publishArtifact in Test := false,
-
-    sonatypeProfileName := "io.paradoxical",
-
-    pgpPublicRing := new File(".deployment/gpg/paradoxical-io.pubgpg"),
-    pgpSecretRing := new File(".deployment/gpg/paradoxical-io-private.gpg"),
-    pgpPassphrase := Some(sys.env.getOrElse("GPG_PASSWORD", default = "").toArray),
 
     pomIncludeRepository := { _ => false },
 
